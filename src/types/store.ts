d56ad5593c5825bb631bc7ce1686c6ec5042d11f
leesuin <suin0212@gmail.com>
2024-04-11 16:51:03 +0900

    [#3] feat: store info api 추가

--- src/types/store.ts
+++ src/types/store.ts
@@ -1,12 +1,27 @@
+export type Menu = {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+  order_option: [];
+};
+
 export type StoreInfo = {
   _id: string;
   name: string;
   image: string;
   review: number;
   tip: number;
+  menu: Menu[];
 };
 
 export interface getStoreListResponse {
   result: string;
   store_list: StoreInfo[];
+}
+
+export interface getMenuListResponse {
+  result: string;
+  store_info: StoreInfo;
 }
