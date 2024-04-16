e8b5cb5f1d210b300263e39a6a6c39db0735ac41
leesuin <suin0212@gmail.com>
2024-04-16 15:38:48 +0900

    [#5] feat: recoil store 추가

---
+++ src/stores/options.ts
@@ -0,0 +1,16 @@
+import { atom } from "recoil";
+
+interface SelectedOptions {
+  [category: string]: {
+    id: string;
+    name: string;
+    category: string;
+    price: number;
+    isChecked: boolean;
+  }[];
+}
+
+export const SelectedOptionsState = atom<SelectedOptions>({
+  key: "selectedOptionsState",
+  default: {}
+});
